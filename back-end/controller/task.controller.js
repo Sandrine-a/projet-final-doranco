2d22f0d7ef357d87c4cd26955f3368a55ca35e1e
Sandrine-a <sandrine_a971@yahoo.fr>
2023-01-16 14:43:55 -0400

    ajout du CRUD Tasks

--- back-end/controller/task.controller.js
+++ back-end/controller/task.controller.js
@@ -1,5 +1,20 @@
 const { Task } = require("../models/index");
 // const Employe = require("../models/index").Task;
+
+/**
+ * Recupere la liste des employes en DB et ma retourne au client
+ * @param {Req} req la requete provenant du client
+ * @param {Res} res la reponse a construire et a envoyer au client
+ */
+exports.get_tasks = (req, res) => {
+  Task.findAll()
+    .then((data) => res.json({ data }))
+    .catch((error) =>
+      res.status(500).json({ message: "Erreur lister", error })
+    );
+};
+
+
 /**
  * Permet la creation d'un nouvel employe en DB, si success retourne l'objet persiste
  * @param {Req} req la requete provenant du client
@@ -16,7 +31,6 @@ exports.create_task = (req, res) => {
   if (!title || !day)
     return res.status(400).json({ message: "title or day missing" });
 
-
   // Creer employe a enregistrer
   const task = {
     title,
@@ -29,5 +43,62 @@ exports.create_task = (req, res) => {
     .then((data) => res.status(200).json({ data }))
     .catch((error) =>
       res.status(500).json({ message: "Erreur create in DB ", error })
+    );
+};
+
+/**
+ * Permet la creation d'un nouvel employe en DB, si success retourne l'objet persiste
+ * @param {Req} req la requete provenant du client
+ * @param {Res} res la reponse a construire et a envoyer au client
+ */
+exports.update_task = (req, res) => {
+  const id = Number(req.params.id); //Recuperation du id du parametre url
+
+  // Valider la requete
+  if (!req.body.title || !req.body.day || !id)
+    return res.status(400).json({ message: "Title, day or id invalid" });
+
+  Task.update(req.body, { where: { id } })
+    .then((number) => {
+      if (number == 1) {
+        res.json({ message: "Update success" });
+      } else {
+        res.json({
+          message: `Failed to update taks with id = ${id}`,
+        });
+      }
+    })
+    .catch((error) =>
+      res
+        .status(500)
+        .json({ message: `Erreur update employe avec id = ${id}`, error })
+    );
+}
+
+/**
+ * Permet la suppression d'un employe en DB
+ * @param {Req} req la requete provenant du client
+ * @param {Res} res la reponse a construire et a envoyer au clien
+ */
+exports.delete_task = (req, res) => {
+  const id = Number(req.params.id); //Recuperation du id du parametre url
+
+  if (!id) { 
+    return res.status(400).json({ message: "Invalid id" });
+  }
+
+  Task.destroy({ where: { id } })
+    .then((number) => {
+      if (number == 1) {
+        res.json({ message: "Delete success" });
+      } else {
+        console.log(number);
+        res.json({ message: `Failed to find id = ${id} introuvable` });
+      }
+    })
+    .catch((error) =>
+      res
+        .status(500)
+        .json({ message: `Failed with task id = ${id}`, error })
     );
 };
--- back-end/router/task.routes.js
+++ back-end/router/task.routes.js
@@ -3,9 +3,9 @@ const router = require("express").Router();
 const task_ctrl = require("../controller/task.controller");
 
 // Enregistement des endpoints et redirection des requetes vers les methodes du controller
-// router.get("/", task_ctrl.get_tasks);
+router.get("/", task_ctrl.get_tasks);
 router.post("/", task_ctrl.create_task);
-// router.put("/:id", employe_ctrl.update_employe);
-// router.delete("/:id", employe_ctrl.delete_employe);
+router.put("/:id", task_ctrl.update_task);
+router.delete("/:id", task_ctrl.delete_task);
 
 module.exports = router;
